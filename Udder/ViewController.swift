78c54a45349b081c70ac7aff4f5fdc6f48533abc
ping john layson <pingjohnlayson@pings-macbook-pro.local>
2018-12-10 12:22:59 +0800

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by ping john layson on 10/12/18.
+//  Copyright © 2018 ping john layson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
